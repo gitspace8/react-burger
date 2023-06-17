c3d983ffb4d7c7110e898d148ce912254a7e908d
Dmitriy Dupliy <dupliy.dv@gmail.com>
2023-06-17 19:18:25 +0300

    кусок переделываем левую часть блока - в2 - вроде работает

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,20 +2,27 @@ import React, {useEffect} from 'react';
 import moduleStyles from './app.module.css';
 import AppHeader from "../app-header/AppHeader";
 import BurgerIngredients from "../burger-ingredients/BurgerIngredients";
-import BurgerConstructor from "../burger-constructor/BurgerConstructor";
 import Margin from "../margin/Margin";
 import {DndProvider} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {getIngredients} from "../../services/actions/burger-ingredients";
+import Modal from "../modal/Modal";
+import IngrDetail from "../burger-ingredients/ingr-detail/IngrDetail";
+import {CLOSE_INGR_DETAILS_MODAL, UNSELECT_INGREDIENT} from "../../services/actions/ingr-details";
 
 const App = () => {
     const dispatch = useDispatch();
+    const ingredientDetailsModal = useSelector(state => state.ingredientDetails.modalIsOpen);
 
     useEffect(() => {
         dispatch(getIngredients())
     }, [dispatch]);
 
+    function handleCloseIngrDetailsModal() {
+        dispatch({type: CLOSE_INGR_DETAILS_MODAL});
+        dispatch({type: UNSELECT_INGREDIENT});
+    }
 
     return (<>
         <div className={moduleStyles.main}>
@@ -28,6 +35,13 @@ const App = () => {
                 </DndProvider>
             </div>
         </div>
+        {
+            ingredientDetailsModal && (
+                <Modal onClose={handleCloseIngrDetailsModal} title={'Детали ингредиента'}>
+                    <IngrDetail/>
+                </Modal>
+            )
+        }
     </>)
 }
 export default App
--- src/components/burger-ingredients/BurgerIngredients.jsx
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -2,30 +2,24 @@ import React from 'react';
 import moduleStyles from './burger-ingredients.module.css'
 import {INGREDIENT_TYPE, INGREDIENTS_TITLES} from "../../utils/config";
 import IngrTab from "./ingr-tab/IngrTab";
-import Modal from "../modal/Modal";
 import IngrList from "./Ingr-list/IngrList";
-import IngrDetail from "./ingr-detail/IngrDetail";
 import {useDispatch, useSelector} from "react-redux";
 import {OPEN_INGR_DETAILS_MODAL, selectIngredient} from "../../services/actions/ingr-details";
 import {CHANGE_TAB} from "../../services/actions/burger-ingredients";
 
 const BurgerIngredients = () => {
     const dispatch = useDispatch();
-    const { ingredients } = useSelector(state => state.burgerIngredients);
-
+    const {ingredients} = useSelector(state => state.burgerIngredients);
     const bunRef = React.useRef(null);
     const sauceRef = React.useRef(null);
     const mainRef = React.useRef(null);
-
     const bun = React.useMemo(() => ingredients.filter((ingredient) => ingredient.type === INGREDIENT_TYPE.BUN), [ingredients]);
     const sauce = React.useMemo(() => ingredients.filter((ingredient) => ingredient.type === INGREDIENT_TYPE.SAUCE), [ingredients]);
     const main = React.useMemo(() => ingredients.filter((ingredient) => ingredient.type === INGREDIENT_TYPE.MAIN), [ingredients]);
 
-
     function handleTabClick(tab) {
         dispatch({
-            type: CHANGE_TAB,
-            tab,
+            type: CHANGE_TAB, tab,
         });
         if (tab === INGREDIENTS_TITLES.MAIN) {
             mainRef.current.scrollIntoView({
@@ -54,7 +48,7 @@ const BurgerIngredients = () => {
 
     return (<section className={moduleStyles.mainContainer}>
         <h1 className="text text_type_main-large mb-5">Соберите бургер</h1>
-        <IngrTab current={INGREDIENTS_TITLES.BUN} onClick={handleTabClick}/>
+        <IngrTab onClick={handleTabClick}/>
         <div className={`${moduleStyles.ingredientsContainer} custom-scroll mt-10 pr-2`}>
             <p className="text text_type_main-medium" ref={bunRef}>{INGREDIENTS_TITLES.BUN}</p>
             <IngrList ingredients={bun} onSelect={handleIngredientClick}/>
